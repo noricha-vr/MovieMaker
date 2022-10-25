9d9359fd6c20aa7dade76cf13dcf2fb633fa5f0a
noricha-vr <noricha.vr@gmail.com>
2022-10-25 16:44:18 +0900

    add author_email and description_file.

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup,find_packages
 
 setup(
     name="MovieMaker",
+    description_file="README.md",
     version="0.0.1",
     license='MIT',
     author="Noricha",
+    author_email="noricha.vr@gmail.com",
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
