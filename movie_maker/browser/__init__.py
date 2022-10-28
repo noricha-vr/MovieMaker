418254e64513967290ef08f4ec747272825a73c4
noricha-vr <noricha.vr@gmail.com>
2022-10-28 23:28:37 +0900

    add __all__ = [     "BaseBrowser",     "BrowserCreator",     "GeneralBrowser",     "TwitterBrowser", ]

--- movie_maker/browser/__init__.py
+++ movie_maker/browser/__init__.py
@@ -2,3 +2,10 @@ from movie_maker.browser.base_browser import BaseBrowser
 from movie_maker.browser.browser_creator import BrowserCreator
 from movie_maker.browser.general_browser import GeneralBrowser
 from movie_maker.browser.twitter_browser import TwitterBrowser
+
+__all__ = [
+    "BaseBrowser",
+    "BrowserCreator",
+    "GeneralBrowser",
+    "TwitterBrowser",
+]
