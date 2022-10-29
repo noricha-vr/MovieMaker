05e91ee71ab3ceab556f858c905141071de2609c
noricha-vr <noricha.vr@gmail.com>
2022-10-29 10:04:28 +0900

    change test url.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name="vrc-movie-maker",
-    version="0.1.1",
+    version="0.1.3",
     license='MIT',
     author="Noricha",
     author_email="noricha.vr@gmail.com",
