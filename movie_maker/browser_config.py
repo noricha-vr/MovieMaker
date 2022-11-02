e1bbe195ba82d841a8e6b27363f9788bb0ed4604
noricha-vr <noricha.vr@gmail.com>
2022-11-02 11:48:33 +0900

    apply dataclass

--- movie_maker/browser_config.py
+++ movie_maker/browser_config.py
@@ -1,24 +1,25 @@
 import hashlib
 from pathlib import Path
 from typing import List
+from dataclasses import dataclass
 
 
+@dataclass
 class BrowserConfig:
     limit_minimum_scroll = 200
     minimum_page_height = 3000
     limit_page_height = 100000
     limit_width = 1920
     limit_height = 1920
+    url: str = ''
+    width: int = 1280
+    height: int = 720
+    max_page_height: int = 50000
+    scroll_each: int = 200
+    targets: List[str] = None
 
-    def __init__(self, url: str = '', width: int = 1280, height: int = 720,
-                 max_page_height: int = 50000, scroll_each: int = 200, targets: List[str] = None):
-        self.url = url
-        if url != '': self.domain = url.split("/")[2]
-        self.width = width
-        self.height = height
-        self.max_page_height = max_page_height
-        self.scroll_each = scroll_each
-        self.targets = targets
+    def __post_init__(self):
+        if self.url != '': self.domain = self.url.split("/")[2]
         self.params_hash = self.params_to_hash()
         self.movie_path = Path(f"movie/{self.params_hash}.mp4")
         self.driver_path = None
