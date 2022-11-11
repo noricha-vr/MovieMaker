d355d05bc0110665cbcf157629c3c5d3321a1f5e
noricha-vr <noricha.vr@gmail.com>
2022-11-11 13:49:21 +0900

    undo ffmpeg setting

--- tests/test_movie_maker.py
+++ tests/test_movie_maker.py
@@ -19,8 +19,8 @@ class TestMovieMaker:
         # ("https://gigazine.net/news/20221012-geforce-rtx-4090-benchmark/", 2000, 2000, 2000, 100, 2),  # Limit test
         # ("https://forest.watch.impress.co.jp/docs/serial/sspcgame/1436345.html", 720, 1280, 5000, 500, 9),
         (
-        "https://www.google.com/maps/place/HUB%E7%A7%8B%E8%91%89%E5%8E%9F%E5%BA%97/@35.700164,138.7193184,9z/data=!3m1!5s0x60188ea7985323af:0x1c68bb773edd834a!4m9!1m2!2m1!1z56eL44OP44OW!3m5!1s0x60188ea7bd68888d:0x25f90a2ae6b34060!8m2!3d35.700164!4d139.7740059!15sCgnnp4vjg4_jg5YiA4gBAVoMIgrnp4sg44OP44OWkgEDcHVimgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVVE0YkZsbVFuZG5SUkFC4AEA?hl=ja",
-        720, 1280, 5000, 500, 9),
+                "https://www.google.com/maps/place/HUB%E7%A7%8B%E8%91%89%E5%8E%9F%E5%BA%97/@35.700164,138.7193184,9z/data=!3m1!5s0x60188ea7985323af:0x1c68bb773edd834a!4m9!1m2!2m1!1z56eL44OP44OW!3m5!1s0x60188ea7bd68888d:0x25f90a2ae6b34060!8m2!3d35.700164!4d139.7740059!15sCgnnp4vjg4_jg5YiA4gBAVoMIgrnp4sg44OP44OWkgEDcHVimgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVVE0YkZsbVFuZG5SUkFC4AEA?hl=ja",
+                720, 1280, 5000, 500, 1),
         (
                 "https://www.google.com/search?q=VRChat&tbm=isch&ved=2ahUKEwij_POxpaX7AhXwS_UHHWbpBQgQ2-cCegQIABAA&oq=VRChat&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIHCAAQBBCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoECCMQJzoLCAAQgAQQsQMQsQM6BwgjEOoCECc6CAgAEIAEELEDOgsIABAEEIAEECUQIDoNCAAQBBCABBAlECAQGDoPCAAQBBCABBAKECUQIBAYUMkEWPMTYOsWaAFwAHgBgAGHA4gBqwqSAQcxLjIuMi4xmAEAoAEBqgELZ3dzLXdpei1pbWewAQfAAQE&sclient=img&ei=rs9tY6PtMPCX1e8P5tKXQA&bih=1041&biw=1252&rlz=1C5GCEM_enJP1012JP1012",
                 720, 1280, 5000, 500, 9),
