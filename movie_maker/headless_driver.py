7362b5521445dd436a2914753ad005ecad1128c6
noricha-vr <noricha.vr@gmail.com>
2022-11-12 15:09:50 +0900

    add browser locale setting.

--- movie_maker/headless_driver.py
+++ movie_maker/headless_driver.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 
-def create_headless_chromedriver(width: int = 1280, height: int = 720, driver_path: str = None) -> webdriver.Chrome:
+def create_headless_chromedriver(width: int = 1280, height: int = 720,
+                                 locale='en_US', driver_path: str = None) -> webdriver.Chrome:
+    """
+    Create headless Chrome driver.
+    :param width: width of browser
+    :param height: height of browser
+    :param locale: locale of browser
+    :param driver_path: path of driver
+    :return: Chrome driver
+    """
     # timeout
     page_load_timeout = 20
     # The following options are required to make headless Chrome
@@ -18,6 +28,8 @@ def create_headless_chromedriver(width: int = 1280, height: int = 720, driver_pa
     chrome_options.add_argument(
         "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
     # Initialize a new browser
+    # Change local setting on pc
+    os.environ['LANG'] = f'{locale}.UTF-8'
     driver = webdriver.Chrome(ChromeDriverManager(path=driver_path).install(), chrome_options=chrome_options)
     driver.set_page_load_timeout(page_load_timeout)
     return driver
