9fad5c0a556f8fc97adc9af8fe7efab5fd17c3bf
noricha-vr <noricha.vr@gmail.com>
2022-11-13 17:14:56 +0900

    version="0.2.2"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vrc-movie-maker",
-    version="0.2.1",
+    version="0.2.2",
     license='MIT',
     author="Noricha",
     author_email="noricha.vr@gmail.com",
