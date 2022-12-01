a1807499c6ed11c040d5f30511c9e3d6d6b91ad4
noricha-vr <noricha.vr@gmail.com>
2022-12-01 13:11:10 +0900

    Fix: Chrome driver version error. Specify version version='107.0.5304.62'

--- movie_maker/headless_driver.py
+++ movie_maker/headless_driver.py
@@ -29,8 +29,11 @@ def create_headless_chromedriver(browser_config: BrowserConfig) -> webdriver:
     chrome_options.add_argument(
         "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
     # Initialize a new browser. Browser type is Brave.
-    driver = webdriver.Chrome(
-        ChromeDriverManager(path=browser_config.driver_path, chrome_type=ChromeType.BRAVE).install(),
-        chrome_options=chrome_options)
+    # driver = webdriver.Chrome(
+    #     ChromeDriverManager(path=browser_config.driver_path, chrome_type=ChromeType.BRAVE).install(),
+    #     chrome_options=chrome_options)
+    driver = webdriver.Chrome(ChromeDriverManager(
+        path=browser_config.driver_path, version='107.0.5304.62', chrome_type=ChromeType.BRAVE).install(),
+                              chrome_options=chrome_options)
     driver.set_page_load_timeout(browser_config.page_load_timeout)
     return driver
